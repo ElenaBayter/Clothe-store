e1041dded8a153ed899cc0f5691265fe62d635bb
Rafael bayter <nafanichka.ev@gmail.com>
2023-11-30 15:34:52 -0500

    HW_6

---
+++ data.js
@@ -0,0 +1,56 @@
+const goodsData = `
+[
+    {
+        "src": "img/1.png",
+        "alt": "ELLERY X M'O CAPSULE",
+        "title": "ELLERY X M'O CAPSULE",
+        "href": "#",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "52.00"
+    },
+    {
+        "src": "img/2.png",
+        "alt": "ELLERY X M'O CAPSULE",
+        "title": "ELLERY X M'O CAPSULE",
+        "href": "#",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "52.00"
+    },
+    {
+        "src": "img/3.png",
+        "alt": "ELLERY X M'O CAPSULE",
+        "title": "ELLERY X M'O CAPSULE",
+        "href": "#",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "52.00"
+    },
+    {
+        "src": "img/4.png",
+        "alt": "ELLERY X M'O CAPSULE",
+        "title": "ELLERY X M'O CAPSULE",
+        "href": "#",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "52.00"
+    },
+    {
+        "src": "img/5.png",
+        "alt": "ELLERY X M'O CAPSULE",
+        "title": "ELLERY X M'O CAPSULE",
+        "href": "#",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "52.00"
+    },
+    {
+        "src": "img/1.png",
+        "alt": "ELLERY X M'O CAPSULE",
+        "title": "ELLERY X M'O CAPSULE",
+        "href": "#",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "52.00"
+    }
+]`;
+// console.log(goodsData);
+// const data = JSON.parse(goodsData);
+// data.forEach((element) => {
+//   console.log(element.img);
+// });
